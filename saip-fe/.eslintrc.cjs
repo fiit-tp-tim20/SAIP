7dde131f9140674081463e9d9ab110a5a474ec57
Lukáš Peťko <luke.petko@gmail.com>
2022-10-19 15:10:27 +0200

    [FE] - eslint testing

--- saip-fe/.eslintrc.cjs
+++ saip-fe/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:react/recommended',
@@ -16,8 +16,8 @@ module.exports = {
     project: './tsconfig.json',
   },
   plugins: [
-    'react',
+    'react'
   ],
   rules: {
-  },
-};
+  }
+}
