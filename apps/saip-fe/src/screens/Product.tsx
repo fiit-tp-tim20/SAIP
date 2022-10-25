20b7821fd3ff8bb988a74c4b12d418ea5186a364
Lukáš Peťko <luke.petko@gmail.com>
2022-10-25 14:14:49 +0200

    [FE] - product text styling

--- apps/saip-fe/src/screens/Product.tsx
+++ apps/saip-fe/src/screens/Product.tsx
@@ -6,12 +6,12 @@ function Product() {
 
 	return (
 		<div className="max-w-7xl">
-			<div className="grid grid-cols-2 gap-6 p-6">
+			<h1 className="p-6 pl-12">{t("product.title") as string}</h1>
+			<div className="grid grid-cols-2 gap-6 px-6">
 				<div className="flex flex-col background-container p-6 rounded-2xl">
-					<h1>{t("product.title") as string}</h1>
 					<div className="py-4">
 						<h2>{t("product.description.title") as string}</h2>
-						<p>{t("product.description.text") as string}</p>
+						<p className="pt-1">{t("product.description.text") as string}</p>
 					</div>
 
 					<div className="py-4">
