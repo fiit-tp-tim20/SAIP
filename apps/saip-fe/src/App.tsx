ad82c59c49b9220bd88a9ac889c40e391d4d9664
Lukáš Peťko <luke.petko@gmail.com>
2022-10-25 14:14:59 +0200

    [FE] - react query dev tools

--- apps/saip-fe/src/App.tsx
+++ apps/saip-fe/src/App.tsx
@@ -6,23 +6,30 @@ import "./i18n";
 import Plan from "./screens/Plan";
 import Devtools from "./dev/Devtools";
 import Product from "./screens/Product";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
 		<>
-			<Suspense>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/plan" element={<Plan />} />
-						<Route path="/news" element={<p>Novinky</p>} />
-						<Route path="/sandbox" element={<Sandbox />} />
-						<Route path="/product" element={<Product />} />
-						<Route path="/" element={<Navigate to="/plan" replace />} />
-						<Route path="*" element={<p>404</p>} />
-					</Routes>
-				</BrowserRouter>
-			</Suspense>
-			<Devtools />
+			<QueryClientProvider client={queryClient}>
+				<Suspense>
+					<BrowserRouter>
+						<Routes>
+							<Route path="/plan" element={<Plan />} />
+							<Route path="/news" element={<p>Novinky</p>} />
+							<Route path="/sandbox" element={<Sandbox />} />
+							<Route path="/product" element={<Product />} />
+							<Route path="/" element={<Navigate to="/plan" replace />} />
+							<Route path="*" element={<p>404</p>} />
+						</Routes>
+					</BrowserRouter>
+				</Suspense>
+				<Devtools />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</>
 	);
 }
