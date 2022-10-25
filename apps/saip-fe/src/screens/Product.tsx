7256a31ea761a4f0801634e13d327d2d08dc7081
Lukáš Peťko <luke.petko@gmail.com>
2022-10-25 10:17:35 +0200

    [FE] - product description layout

--- apps/saip-fe/src/screens/Product.tsx
+++ apps/saip-fe/src/screens/Product.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 function Product() {
+	const { t } = useTranslation();
+
 	return (
-		<div className="flex flex-row">
-			<div className="flex flex-col background-container">
-				<h1>Product</h1>
+		<div className="max-w-7xl">
+			<div className="grid grid-cols-2 gap-6 p-6">
+				<div className="flex flex-col background-container p-6 rounded-2xl">
+					<h1>{t("product.title") as string}</h1>
+					<h2>{t("product.description.title") as string}</h2>
+					<p>{t("product.description.text") as string}</p>
+
+					<h2>{t("product.features.title") as string}</h2>
+				</div>
+				<div className="flex flex-col rounded-2xl">
+					<img src="https://via.placeholder.com/1280" alt="placeholder" />
+				</div>
 			</div>
-			<div className="flex flex-col"></div>
 		</div>
 	);
 }
