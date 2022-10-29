ce8335a7e11dea4a4373f550bc9706af87506591
Lukáš Peťko <luke.petko@gmail.com>
2022-10-29 21:42:53 +0200

    [FE] - threejs refactor

--- apps/saip-fe/src/components/three/Bike.tsx
+++ apps/saip-fe/src/components/three/Bike.tsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { Vector3 } from "three";
+import { Group, Mesh, Vector3 } from "three";
 
-type Props = {
-	position: Vector3;
-};
+function Bike() {
+	const { nodes } = useGLTF("/bike_test.glb");
 
-function Bike(props: Props) {
-	const { position } = props;
-	const { nodes } = useGLTF("/bike.glb");
+	const upgradeRef = useRef<Mesh>(null!);
+
+	// hide upgradeRef.current after 5 seconds
+	useEffect(() => {
+		console.warn(nodes);
+		console.warn(upgradeRef.current);
+		console.log(upgradeRef.current.parent?.children);
+		let isVisible = upgradeRef.current.visible;
+		setInterval(() => {
+			upgradeRef.current.visible = !isVisible;
+			isVisible = !isVisible;
+		}, 1000);
+	}, []);
 
 	return (
-		<mesh position={position} scale={[2, 2, 2]} geometry={nodes.Bike.geometry}>
-			<meshToonMaterial attach="material" color="gray" />
-		</mesh>
+		<>
+			<mesh scale={[2, 2, 2]} geometry={nodes.Bike.geometry}>
+				<meshToonMaterial attach="material" color="gray" />
+			</mesh>
+			<mesh scale={[2, 2, 2]} geometry={nodes.Upgrade_1.geometry} ref={upgradeRef}>
+				<meshToonMaterial attach="material" color="hotpink" />
+			</mesh>
+			<mesh scale={[2, 2, 2]} geometry={nodes.test_cube.geometry}>
+				<meshToonMaterial attach="material" color="hotpink" />
+			</mesh>
+		</>
 	);
 }
 
--- apps/saip-fe/src/components/three/Canvas.tsx
+++ apps/saip-fe/src/components/three/Canvas.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { PresentationControls } from "@react-three/drei";
 import Bike from "./Bike";
+import { Vector3 } from "three";
 
 export default function Test() {
 	return (
@@ -16,8 +17,8 @@ export default function Test() {
 				polar={[0, Math.PI / 4]}
 				azimuth={[-Math.PI / 4, Math.PI / 4]}
 			>
-				<group position-y={-0.75} dispose={null}>
-					<Bike position={[0, 0.5, 0]} />
+				<group dispose={null} position={new Vector3(0, -0.25, 0)}>
+					<Bike />
 				</group>
 			</PresentationControls>
 		</Canvas>
