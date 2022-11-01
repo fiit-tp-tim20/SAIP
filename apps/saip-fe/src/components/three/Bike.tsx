39c0091f72ea79208e2d5adfe48e1199b15db12e
Lukáš Peťko <luke.petko@gmail.com>
2022-11-01 18:39:49 +0100

    [FE] - animations on models

--- apps/saip-fe/src/components/three/Bike.tsx
+++ apps/saip-fe/src/components/three/Bike.tsx
@@ -1,43 +1,111 @@
 import * as THREE from "three";
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useSpring, a } from "@react-spring/three";
 
 function Bike(props: JSX.IntrinsicElements["group"]) {
 	const { nodes, materials } = useGLTF("/bike_v8_test.glb");
+	const group = useRef<THREE.Group>(null!);
+
+	const [spring, api] = useSpring(() => ({ rotation: [0, 0, 0], config: { mass: 5, tension: 100 } }), []);
+
+	useEffect(() => {
+		let timeout: any;
+		const rotate = () => {
+			api.start({ rotation: [Math.random() * Math.PI * 3, 0, 0] });
+			timeout = setTimeout(rotate, (0.5 + Math.random() * 2) * 1000);
+		};
+		rotate();
+		return () => void clearTimeout(timeout);
+	}, []);
+
 	return (
 		<group {...props} dispose={null}>
+			<a.group name="Wheel_Front" ref={group} position={[0, 0.87, 1.44]} {...spring}>
+				<mesh
+					name="Hub_front"
+					geometry={nodes.Hub_front.geometry}
+					material={materials["Material.002"]}
+					// position={[0, 0.87, 1.44]}
+					scale={[1.54, 1, 1]}
+				/>
+				<mesh
+					name="Spoke_1"
+					geometry={nodes.Spoke_1.geometry}
+					material={materials["Material.002"]}
+					// position={[0, 0.87, 1.44]}
+					rotation={[-Math.PI, 0, 0]}
+					scale={[-0.9, -1, -1]}
+				/>
+				<mesh
+					name="Torus001"
+					geometry={nodes.Torus001.geometry}
+					material={materials.Saddle}
+					// position={[0, 0.87, 1.44]}
+					rotation={[0, 0, -Math.PI / 2]}
+				/>
+			</a.group>
+			<a.group name="Wheel_Back" ref={group} position={[0, 0.87, -1.48]} {...spring}>
+				<mesh
+					name="Hub_back"
+					geometry={nodes.Hub_back.geometry}
+					material={materials["Material.002"]}
+					// position={[0, 0.87, -1.48]}
+					scale={[1.47, 1, 1]}
+				/>
+				<mesh
+					name="Spoke_2"
+					geometry={nodes.Spoke_2.geometry}
+					material={materials["Material.002"]}
+					// position={[0, 0.87, -1.48]}
+					rotation={[-Math.PI, 0, 0]}
+					scale={[-0.48, -1, -1]}
+				/>
+				<mesh
+					name="Torus002"
+					geometry={nodes.Torus002.geometry}
+					material={materials.Saddle}
+					// position={[0, 0.87, -1.48]}
+					rotation={[0, 0, -Math.PI / 2]}
+				/>
+			</a.group>
+			<a.group name="Paddles" position={[0, 0.77, -0.31]} {...spring}>
+				<mesh
+					name="Crank_arm_2"
+					geometry={nodes.Crank_arm_2.geometry}
+					material={materials["Material.002"]}
+					// position={[0, 0.77, -0.31]}
+					rotation={[0.29, 0, -1.54]}
+					scale={0.06}
+				/>
+				<mesh
+					name="Crank_arm_1"
+					geometry={nodes.Crank_arm_1.geometry}
+					material={materials["Material.002"]}
+					// position={[0, 0.77, -0.31]}
+					rotation={[0.29, 0, 1.6]}
+					scale={0.06}
+				/>
+				<mesh
+					name="Paddle_1"
+					geometry={nodes.Paddle_1.geometry}
+					material={materials.Saddle}
+					position={[0.33, -0.37, -0.1]}
+					rotation={[0, 0, 0.03]}
+					scale={[0.09, 0.11, 0.07]}
+				/>
+				<mesh
+					name="Paddle_2"
+					geometry={nodes.Paddle_2.geometry}
+					material={materials.Saddle}
+					position={[-0.33, 0.32, 0.1]}
+					rotation={[-3.1, 0, -0.03]}
+					scale={[-0.09, -0.11, -0.07]}
+				/>
+			</a.group>
 			<mesh name="Top_Seat_tube" geometry={nodes.Top_Seat_tube.geometry} material={materials.Main} />
 			<mesh name="Fort_Seat_stay" geometry={nodes.Fort_Seat_stay.geometry} material={materials.Main} />
-			<mesh
-				name="Hub_back"
-				geometry={nodes.Hub_back.geometry}
-				material={materials["Material.002"]}
-				position={[0, 0.87, -1.48]}
-				scale={[1.47, 1, 1]}
-			/>
-			<mesh
-				name="Hub_front"
-				geometry={nodes.Hub_front.geometry}
-				material={materials["Material.002"]}
-				position={[0, 0.87, 1.44]}
-				scale={[1.54, 1, 1]}
-			/>
-			<mesh
-				name="Spoke_2"
-				geometry={nodes.Spoke_2.geometry}
-				material={materials["Material.002"]}
-				position={[0, 0.87, -1.48]}
-				rotation={[-Math.PI, 0, 0]}
-				scale={[-0.48, -1, -1]}
-			/>
-			<mesh
-				name="Spoke_1"
-				geometry={nodes.Spoke_1.geometry}
-				material={materials["Material.002"]}
-				position={[0, 0.87, 1.44]}
-				rotation={[-Math.PI, 0, 0]}
-				scale={[-0.9, -1, -1]}
-			/>
 			<mesh name="Stem" geometry={nodes.Stem.geometry} material={materials.Main} position={[0, 1.97, 0.74]} />
 			<mesh
 				name="Handlebars"
@@ -89,38 +157,6 @@ function Bike(props: JSX.IntrinsicElements["group"]) {
 				scale={0.06}
 			/>
 			<mesh
-				name="Crank_arm_2"
-				geometry={nodes.Crank_arm_2.geometry}
-				material={materials["Material.002"]}
-				position={[0, 0.77, -0.31]}
-				rotation={[0.29, 0, -1.54]}
-				scale={0.06}
-			/>
-			<mesh
-				name="Crank_arm_1"
-				geometry={nodes.Crank_arm_1.geometry}
-				material={materials["Material.002"]}
-				position={[0, 0.77, -0.31]}
-				rotation={[0.29, 0, 1.6]}
-				scale={0.06}
-			/>
-			<mesh
-				name="Paddle_1"
-				geometry={nodes.Paddle_1.geometry}
-				material={materials.Saddle}
-				position={[0.33, 0.45, -0.41]}
-				rotation={[0, 0, 0.03]}
-				scale={[0.09, 0.11, 0.07]}
-			/>
-			<mesh
-				name="Paddle_2"
-				geometry={nodes.Paddle_2.geometry}
-				material={materials.Saddle}
-				position={[-0.33, 1.06, -0.22]}
-				rotation={[-3.1, 0, -0.03]}
-				scale={[-0.09, -0.11, -0.07]}
-			/>
-			<mesh
 				name="Chain"
 				geometry={nodes.Chain.geometry}
 				material={materials["Material.002"]}
@@ -137,20 +173,6 @@ function Bike(props: JSX.IntrinsicElements["group"]) {
 				scale={0.06}
 			/>
 			<mesh name="Head_tube" geometry={nodes.Head_tube.geometry} material={materials.Main} />
-			<mesh
-				name="Torus001"
-				geometry={nodes.Torus001.geometry}
-				material={materials.Saddle}
-				position={[0, 0.87, 1.44]}
-				rotation={[0, 0, -Math.PI / 2]}
-			/>
-			<mesh
-				name="Torus002"
-				geometry={nodes.Torus002.geometry}
-				material={materials.Saddle}
-				position={[0, 0.87, -1.48]}
-				rotation={[0, 0, -Math.PI / 2]}
-			/>
 			<mesh name="Fort_Seat_stay001" geometry={nodes.Fort_Seat_stay001.geometry} material={materials.Main} />
 			<mesh
 				name="Screen_body"
@@ -172,6 +194,6 @@ function Bike(props: JSX.IntrinsicElements["group"]) {
 	);
 }
 
-useGLTF.preload("/bike_v8_test-transformed.glb");
+useGLTF.preload("/bike_v8_test.glb");
 
 export default Bike;
