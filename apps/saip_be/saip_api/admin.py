a7f8eb672556f948555bc1bb3c937d5fe8b17d11
Radovan Tvrdak <radovan.tvrdak@gmai.com>
2022-11-02 21:28:52 +0100

    [BE] - Added the models to admin interface and changed names to be singular.

--- apps/saip_be/saip_api/admin.py
+++ apps/saip_be/saip_api/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Turn, Company, Production, Spending, Product, Factory, CompaniesState, MarketState, Parameter,\
+    EmailGroup
 
-# Register your models here.
+
+@admin.register(Turn)
+class TurnsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+
+
+@admin.register(Production)
+class ProductionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Spending)
+class SpendingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Factory)
+class FactoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CompaniesState)
+class CompaniesStateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MarketState)
+class MarketStateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EmailGroup)
+class EmailGroupAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email')
+
--- apps/saip_be/saip_api/models.py
+++ apps/saip_be/saip_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Turns(models.Model):
+class Turn(models.Model):
     number = models.PositiveIntegerField(null=True)
     start = models.DateTimeField(null=True, auto_now_add=True)
     end = models.DateTimeField(null=True, blank=True)
@@ -12,15 +12,16 @@ class Turns(models.Model):
         get_latest_by = 'start'
 
 
-class Companies(models.Model):
+class Company(models.Model):
     name = models.CharField(max_length=100, null=True)
     user = models.ForeignKey(User, models.DO_NOTHING, null=True, related_name='user_companies')
 
     class Meta:
         db_table = 'Companies'
+        verbose_name_plural = 'Companies'
 
 
-class Productions(models.Model):
+class Production(models.Model):
     man_cost = models.FloatField(null=True)
     sell_price = models.FloatField(null=True)
     volume = models.PositiveIntegerField(null=True)
@@ -29,7 +30,7 @@ class Productions(models.Model):
         db_table = 'Productions'
 
 
-class Spendings(models.Model):
+class Spending(models.Model):
     r_d = models.FloatField(null=True)
     marketing = models.FloatField(null=True)
     factory = models.FloatField(null=True)
@@ -39,29 +40,30 @@ class Spendings(models.Model):
         db_table = 'Spendings'
 
 
-class Products(models.Model):
+class Product(models.Model):
     upgrades = models.IntegerField(null=True)
 
     class Meta:
         db_table = 'Products'
 
 
-class Factories(models.Model):
+class Factory(models.Model):
     employees = models.PositiveIntegerField(null=True)
     capacity = models.PositiveIntegerField(null=True)
     base_cost = models.FloatField(null=True)
 
     class Meta:
         db_table = 'Factories'
+        verbose_name_plural = 'Factories'
 
 
-class CompaniesStates(models.Model):
-    company = models.ForeignKey(Companies, models.DO_NOTHING, null=True)
-    turn = models.ForeignKey(Turns, models.DO_NOTHING, null=True)
-    production = models.OneToOneField(Productions, models.DO_NOTHING, null=True)
-    spending = models.OneToOneField(Spendings, models.DO_NOTHING, null=True)
-    product = models.OneToOneField(Products, models.DO_NOTHING, null=True)
-    factory = models.OneToOneField(Factories, models.DO_NOTHING, null=True)
+class CompaniesState(models.Model):
+    company = models.ForeignKey(Company, models.DO_NOTHING, null=True)
+    turn = models.ForeignKey(Turn, models.DO_NOTHING, null=True)
+    production = models.OneToOneField(Production, models.DO_NOTHING, null=True)
+    spending = models.OneToOneField(Spending, models.DO_NOTHING, null=True)
+    product = models.OneToOneField(Product, models.DO_NOTHING, null=True)
+    factory = models.OneToOneField(Factory, models.DO_NOTHING, null=True)
     fan_base = models.PositiveIntegerField(null=True, blank=True)
     balance = models.FloatField(null=True, blank=True)
     stock_price = models.FloatField(null=True, blank=True)
@@ -71,8 +73,8 @@ class CompaniesStates(models.Model):
         db_table = 'CompaniesStates'
 
 
-class MarketStates(models.Model):
-    turn = models.ForeignKey(Turns, models.DO_NOTHING, null=True)
+class MarketState(models.Model):
+    turn = models.ForeignKey(Turn, models.DO_NOTHING, null=True)
     size = models.PositiveIntegerField(null=True)
     demand = models.PositiveIntegerField(null=True)
 
@@ -80,8 +82,8 @@ class MarketStates(models.Model):
         db_table = 'MarketStates'
 
 
-class Parameters(models.Model):
-    turn = models.ForeignKey(Turns, models.DO_NOTHING, null=True)
+class Parameter(models.Model):
+    turn = models.ForeignKey(Turn, models.DO_NOTHING, null=True)
     market_size_diff = models.IntegerField(null=True)
     run_cost_multiplier = models.FloatField(null=True)
 
@@ -89,7 +91,7 @@ class Parameters(models.Model):
         db_table = 'Parameters'
 
 
-class Emails(models.Model):
+class EmailGroup(models.Model):
     user = models.ForeignKey(User, models.DO_NOTHING, null=True)
     email = models.EmailField(null=True)
 
