2e4fd98e9eca66c7ec5c437a8086157c7582ee76
Lukáš Peťko <luke.petko@gmail.com>
2022-11-05 22:20:06 +0100

    [FE] - eslint

--- apps/saip-fe/src/components/news/tweet.tsx
+++ apps/saip-fe/src/components/news/tweet.tsx
@@ -3,23 +3,23 @@ import React from "react";
 type Props = {
 	username: string;
 	avatar: string;
-    message: string;
+	message: string;
 };
 
 function Tweet(props: Props) {
-	const { username, avatar, message  } = props;
+	const { username, avatar, message } = props;
 
 	return (
-        <div className="flex flex-col background-container p-3 rounded-2xl">
-            <div className="flex flex-row">
-                    <div className="w-10 h-10 rounded-full bg-gray-200">
-                        <img src={`https://avatars.dicebear.com/api/miniavs/${avatar}.png`} alt="person" />
-                    </div>
-                    <h2 className="pl-2">{username}</h2>
-            </div>
-                <p>{message}</p>    
-        </div>
-    );
+		<div className="flex flex-col background-container p-3 rounded-2xl">
+			<div className="flex flex-row">
+				<div className="w-10 h-10 rounded-full bg-gray-200">
+					<img src={`https://avatars.dicebear.com/api/miniavs/${avatar}.png`} alt="person" />
+				</div>
+				<h2 className="pl-2">{username}</h2>
+			</div>
+			<p>{message}</p>
+		</div>
+	);
 }
 
 export default Tweet;
--- apps/saip-fe/src/screens/News.tsx
+++ apps/saip-fe/src/screens/News.tsx
@@ -1,22 +1,21 @@
 import React from "react";
-import { Divide } from "react-feather";
 import { useTranslation } from "react-i18next";
-import { useQuery } from "react-query";
 import Tweet from "../components/news/tweet";
 
 function News() {
-    const { t } = useTranslation();
-    const text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum modi earum est corporis deserunt error hic, eum praesentium rerum minima, et culpa illo, nisi quasi dicta officia consequuntur! Nesciunt, neque.";
-    
-    return (
-        <div className="max-w-7xl">
+	const { t } = useTranslation();
+	const text =
+		"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum modi earum est corporis deserunt error hic, eum praesentium rerum minima, et culpa illo, nisi quasi dicta officia consequuntur! Nesciunt, neque.";
+
+	return (
+		<div className="max-w-7xl">
 			<h1 className="p-6">{t("news.title") as string}</h1>
-            <div className="grid grid-cols-1 gap-6">
-                <Tweet username={"Pelec"} avatar={"Meh"} message={text}/>
-                <Tweet username={"Floppa"} avatar={"Flop"} message={text}/>
-                <Tweet username={"Jesus Christ"} avatar={"Heh"} message={text}/>
-            </div>
-        </div>
-    );
+			<div className="grid grid-cols-1 gap-6">
+				<Tweet username="Pelec" avatar="Meh" message={text} />
+				<Tweet username="Floppa" avatar="Flop" message={text} />
+				<Tweet username="Jesus Christ" avatar="Heh" message={text} />
+			</div>
+		</div>
+	);
 }
 export default News;
