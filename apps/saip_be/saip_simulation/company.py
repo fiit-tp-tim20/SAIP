f75fc97eeeae0fa9d1779c01b8df883dea25ab2d
vikioza <vikioza@gmail.com>
2022-11-06 21:11:02 +0100

    added config.py to simulation updated factory logic

--- apps/saip_be/saip_simulation/company.py
+++ apps/saip_be/saip_simulation/company.py
@@ -1,12 +1,40 @@
 from dataclasses import dataclass
 
 from product import Product, DailyProduct, LastingProduct
+from config import TURN_LENGTH
+
 
 
 @dataclass
 class Factory:
     capacity: int
-    upkeep: float
+    employees: int  # to be changed to list (or multiple attributes) after we implement different employees
+    employee_salary: float
+    base_energy_cost: float
+    energy_cost_per_machine: float
+    machine_count: int
+    upkeep = {
+        'rent': float,
+        'energy': float,
+        'salaries': float,  # employees * salary * 3 (length of turn)
+        'materials': float  # this one might be irrelevant
+    }
+    
+    def setup_factory(self):
+        pass
+    
+    def update_upkeep(self, new_rent: float = None):
+        if new_rent:
+            self.upkeep['rent'] = new_rent
+        self.upkeep['energy'] = self.__calculate_energies()
+        self.upkeep['salaries'] = self.__calculate_salaries()
+        self.upkeep['materials'] = 0
+    
+    def __calculate_energies(self):
+        return self.base_energy_cost + self.energy_cost_per_machine * self.machine_count
+    
+    def __calculate_salaries(self):
+        return self.employee_salary * self.employees * TURN_LENGTH
 
 
 @dataclass
---
+++ apps/saip_be/saip_simulation/config.py
@@ -0,0 +1,1 @@
+TURN_LENGTH = 3  # turn_length is 1 quarter, so 3 months
