952a048832613b045dad89d8c289d0f3c03dae93
Dewflio <leonard.puskac@gmail.com>
2022-11-06 19:06:02 +0100

    added some methods to simulation

--- apps/saip_be/saip_simulation/simulation.py
+++ apps/saip_be/saip_simulation/simulation.py
@@ -8,5 +8,21 @@ class Simulation:
     companies: Dict[str, Company]
     market: Market
     
+    #ends the turn - and runs the simulation
     def end_turn(self):
+        pass
+
+    #updates the database containing market info
+    #called by class lecturer user
+    def commit_market_state(self):
+        pass
+    
+    #returns information about the state of the market, and new state of the company
+    #returns a json with company_state information
+    def get_company_state(self, user):
+        pass
+    
+    #returns information about the market state
+    #returns json
+    def get_market_state(self):
         pass
