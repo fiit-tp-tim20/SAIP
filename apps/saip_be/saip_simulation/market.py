78584a6f3ba1f3fd1141925b5a648ee64daedd91
Dewflio <leonard.puskac@gmail.com>
2022-11-06 20:06:01 +0100

    market class - set up attributes

--- apps/saip_be/saip_simulation/market.py
+++ apps/saip_be/saip_simulation/market.py
@@ -6,3 +6,15 @@ from customer import Customer
 @dataclass
 class Market:
     customers: List[Customer]
+
+    loan_limit: int         #limit on loans (in dollars/euros)
+    interest_rate: int      #percentage
+    tax_rate: int           #percentage
+
+    marketing_weight: int   #percentage of market that is interesed in marketing
+    rnd_weight: int         #percentage of market that is interesed in rnd
+    price_weight: int       #percentage of market that is interesed in price
+
+    customer_base: int      #overall number of potential customers
+
+    #TODO add sensitivity to attribute(marketing, rnd, price)(maybe)
