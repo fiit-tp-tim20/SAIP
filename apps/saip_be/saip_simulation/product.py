07ce1057160cd41cb3c9626aeb142df8e6267a9b
vikioza <vikioza@gmail.com>
2022-11-06 19:32:56 +0100

    defining product methods

--- apps/saip_be/saip_simulation/product.py
+++ apps/saip_be/saip_simulation/product.py
@@ -3,8 +3,30 @@ from abc import ABC, abstractmethod
 
 @dataclass
 class Product(ABC):
-    upgrade_price: float
     upgrades: dict
+    __price: float
+    __upgrade_price: float
+    
+    def set_price(self, new_price: float) -> None:
+        self.__price = new_price
+        
+    def get_price(self) -> float:
+        return self.__price
+    
+    def __set_upgrade_price(self, new_upgrade_price) -> None:
+        self.__set_upgrade_price = new_upgrade_price
+    
+    def get_upgrade_price(self) -> float:
+        return self.__upgrade_price
+    
+    def upgrade_product(self):
+        # ToDo add upgrade logic here
+        new_upgrade_price = 0
+        self.__set_upgrade_price(new_upgrade_price)
+        
+    def __post_init__(self):
+        if self.__class__ == Product:
+            raise TypeError("Cannot instantiate abstract class.")
 
 
 @dataclass
