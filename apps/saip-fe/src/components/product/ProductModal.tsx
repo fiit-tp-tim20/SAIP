e00cefbcef760c3b20603c802f97534cff946def
Lukáš Peťko <luke.petko@gmail.com>
2022-11-08 12:36:18 +0100

    [FE] - modal changes

--- apps/saip-fe/src/components/product/ProductModal.tsx
+++ apps/saip-fe/src/components/product/ProductModal.tsx
@@ -16,16 +16,54 @@ function ProductModal(props: Props) {
 	console.log(upgrade);
 
 	return (
-		<div className="background-container rounded-2xl py-6 px-3 max-w-7xl" onClick={onClick}>
-			<div className="flex flex-row">
-				<div className="flex flex-col px-3">
-					<h3>{t(`research.features.${upgrade.id}.title`) as string}</h3>
+		<div
+			className="background-container rounded-2xl p-6 grid grid-cols-2 gap-6 max-w-5xl min-h-[50vh] max-h-[95vh]"
+			onClick={onClick}
+		>
+			<div className="flex flex-col max-h-[90vh] overflow-scroll">
+				<div className="py-2">
+					<h2 className="pb-2">{t(`research.features.${upgrade.id}.title`) as string}</h2>
 					<p>{t(`research.features.${upgrade.id}.text`) as string}</p>
+				</div>
+				<div className="py-2">
 					<h4>{t(`research.playerResearched.title`) as string}</h4>
+					{upgrade.players.map((player) => (
+						<div key={player.name} className="flex items-center p-3 my-1 rounded-2xl bg-gray-200">
+							<div className="avatar pr-4">
+								<div className="w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+									<img
+										src={`https://avatars.dicebear.com/api/miniavs/${player.image}.png`}
+										alt={player.name}
+										className="w-8 h-8 rounded-full mr-2"
+									/>
+								</div>
+							</div>
+							<p>{player.name}</p>
+						</div>
+					))}
 				</div>
-				<div className="h-auto px-3">
-					<Canvas cameraPosition={new Vector3(...upgrade.camera.position)} />
+				<div className="py-2 flex flex-row items-center justify-between">
+					<h4>{t(`research.price.title`) as string}</h4>
+					<h5>{upgrade.price}€</h5>
 				</div>
+				{upgrade.progress && (
+					<div className="py-2 items-center">
+						<h4>{t(`research.progress.title`) as string}</h4>
+						<div className="flex flex-row items-center justify-between text-center">
+							<progress
+								className="progress progress-primary w-96"
+								value={upgrade.progress}
+								max={upgrade.price}
+							/>
+							<p className="my-auto ml-2">
+								{upgrade.progress}/{upgrade.price}
+							</p>
+						</div>
+					</div>
+				)}
+			</div>
+			<div className="h-auto max-h-[90vh]">
+				<Canvas cameraPosition={new Vector3(...upgrade.camera.position)} />
 			</div>
 		</div>
 	);
