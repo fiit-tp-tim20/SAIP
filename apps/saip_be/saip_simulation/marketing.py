e246ce56b496d2161039b203c2d39964655477e9
Dewflio <leonard.puskac@gmail.com>
2022-11-15 13:18:57 +0100

    [BE] - adjusted MarketingType class

--- apps/saip_be/saip_simulation/marketing.py
+++ apps/saip_be/saip_simulation/marketing.py
@@ -2,6 +2,16 @@ from dataclasses import dataclass
 
 @dataclass
 class MarketingType:
-    cost: int                   #cost per unit per month
-    amount_invested: int        #total investment of the company into this type of marketing
-    effectivity_modifier: float #the weight of this type of marketing
+    ideal_investment_range: tuple(float, float) # ideal range of investment
+    amount_invested: int                        # total investment of the company into this type of marketing
+    effectivity_modifier: float                 # the weight of this type of marketing
+
+    # getters 
+    def get_ideal_investment_range(self):
+        return self.ideal_investment_range
+
+    def get_amount_invested(self):
+        return self.amount_invested
+
+    def get_effectivity_modifier(self):
+        return self.effectivity_modifier
