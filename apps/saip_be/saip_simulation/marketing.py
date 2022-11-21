079cd301f58bbd53d1cc55f9e1cba52d2d92b48e
vikioza <vikioza@gmail.com>
2022-11-21 21:06:17 +0100

    [BE] moving MarketingType logic to correct branch

--- apps/saip_be/saip_simulation/marketing.py
+++ apps/saip_be/saip_simulation/marketing.py
@@ -2,16 +2,4 @@ from dataclasses import dataclass
 
 @dataclass
 class MarketingType:
-    ideal_investment_range: tuple(float, float) # ideal range of investment
-    amount_invested: int                        # total investment of the company into this type of marketing
-    effectivity_modifier: float                 # the weight of this type of marketing
-
-    # getters 
-    def get_ideal_investment_range(self):
-        return self.ideal_investment_range
-
-    def get_amount_invested(self):
-        return self.amount_invested
-
-    def get_effectivity_modifier(self):
-        return self.effectivity_modifier
+    pass
