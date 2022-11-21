56e210d7e762c7d861df15dcda04fcdbe39e3659
vikioza <vikioza@gmail.com>
2022-11-21 19:04:11 +0100

    [BE] defined min and max marketing investments

--- apps/saip_be/saip_simulation/config.py
+++ apps/saip_be/saip_simulation/config.py
@@ -3,9 +3,19 @@ FACTORY_MAINTENANCE_RATE = 0.05
 
 
 class MarketingModifiers:
-    SOCIAL_MEDIA = 0
+    SOCIAL_MEDIA = 2
     LOCAL_NEWSPAPER = 0
-    BILLBOARD = 0
-    CABLE_NEWS = 0
-    PODCAST = 0
+    BILLBOARD = 1
+    CABLE_NEWS = 1.3
+    PODCAST = 1.5
+    
+class MarketingInvestments:
+    SOCIAL_MEDIA_MIN = 100
+    SOCIAL_MEDIA_MAX = 5000
+    BILLBOARD_MIN = 500
+    BILLBOARD_MAX = 2500
+    CABLE_NEWS_MIN = 2000
+    CABLE_NEWS_MAX = 3500
+    PODCAST_MIN = 1000
+    PODCAST_MAX = 2500
     
