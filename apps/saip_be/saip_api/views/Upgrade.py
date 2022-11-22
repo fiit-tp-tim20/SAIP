947fefb2ffee5e9dbbf217418392091fd52eb54e
y3man <radovan.tvrdak@gmail.com>
2022-11-22 12:01:35 +0100

    [BE] - Replaced JSONResponse with Response

--- apps/saip_be/saip_api/views/Upgrade.py
+++ apps/saip_be/saip_api/views/Upgrade.py
@@ -2,24 +2,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from saip_api.models import Company, Companies_Upgrades, Upgrade, Game
-from django.http import JsonResponse
+
 
 class UpgradeView(APIView):
-    
+
     def get(self, request) -> Response:
 
         if not request.user or not request.user.is_authenticated:
             return Response({"detail": "User is not authenticated"}, status=401)
 
-        company = Company.objects.get(user = request.user)
-        companies_upgrades = Companies_Upgrades.objects.filter(company = company)
+        company = Company.objects.get(user=request.user)
+        companies_upgrades = Companies_Upgrades.objects.filter(company=company)
 
         response = dict()
         response['upgrade'] = list()
         other_companies_list = []
 
         for upgrade in companies_upgrades:
-            other_companies = Companies_Upgrades.objects.filter(upgrade = upgrade.upgrade, status = "f", game = upgrade.game)
+            other_companies = Companies_Upgrades.objects.filter(upgrade=upgrade.upgrade, status="f", game=upgrade.game)
             for item in other_companies:
                 other_companies_list.append(item.company.name)
 
@@ -27,13 +27,13 @@ class UpgradeView(APIView):
             camera_rot = Upgrade.objects.get(pk=upgrade.id).camera_rot.split(",")
 
             response['upgrade'].append({
-                'name':Upgrade.objects.get(pk=upgrade.id).name,
-                'players':other_companies_list,
-                'status':upgrade.get_status_display(),
-                'price':Upgrade.objects.get(pk=upgrade.id).cost,
-                'progress':upgrade.progess,
-                'camera_pos':camera_pos,
-                'camera_rot':camera_rot,
+                'name': Upgrade.objects.get(pk=upgrade.id).name,
+                'players': other_companies_list,
+                'status': upgrade.get_status_display(),
+                'price': Upgrade.objects.get(pk=upgrade.id).cost,
+                'progress': upgrade.progess,
+                'camera_pos': camera_pos,
+                'camera_rot': camera_rot,
             })
 
-        return JsonResponse(response, safe=False)
+        return Response(response)
