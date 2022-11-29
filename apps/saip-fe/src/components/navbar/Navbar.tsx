1f0a36302bc011d7a5970dd0c6cd13c06ccd7ba4
Lukáš Peťko <luke.petko@gmail.com>
2022-11-29 19:43:13 +0100

    [FE] - navbar bg fix

--- apps/saip-fe/src/components/navbar/Navbar.tsx
+++ apps/saip-fe/src/components/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
 	const tabs = ["dashboard", "product", "news", "sandbox"];
 
 	return (
-		<div className="flex flex-row p-2 top-0 w-screen fixed left-0 z-40 border-b-2 border-b-accent-200">
+		<div className="flex flex-row p-2 top-0 w-screen fixed left-0 z-40 border-b-2 border-b-accent-200 bg-white">
 			<p className="px-8 normal-case text-xl align-middle font-bold text-accent-900 leading-[40px]">SAIP</p>
 			<ul className="flex flex-row p-0 m-0">
 				{tabs.map((tab) => (
