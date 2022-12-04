c0f20ef8c0c931635d150e4248ae1723a6ea0233
Lukáš Peťko <luke.petko@gmail.com>
2022-12-04 14:25:59 +0100

    color scheme change

--- apps/saip-fe/src/components/navbar/Navbar.tsx
+++ apps/saip-fe/src/components/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
 	const tabs = ["dashboard", "product", "news"];
 
 	return (
-		<div className="flex flex-row p-2 top-0 w-screen fixed left-0 z-40 border-b-2 border-b-accent-200 bg-white">
+		<div className="flex flex-row p-2 top-0 w-screen fixed left-0 z-40 border-b-2 border-b-accent-700 bg-white">
 			<p className="px-8 normal-case text-xl align-middle font-bold text-accent-800 leading-[40px]">SAIP</p>
 			<ul className="flex flex-row p-0 m-0">
 				{tabs.map((tab) => (
