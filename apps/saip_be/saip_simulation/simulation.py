8f0d672db45ef3d606dc55e515e0525f100490d9
Dewflio <43574262+Dewflio@users.noreply.github.com>
2023-01-22 20:10:23 +0100

    [BE] - added creation of market to sim setup

--- apps/saip_be/saip_simulation/simulation.py
+++ apps/saip_be/saip_simulation/simulation.py
@@ -32,10 +32,12 @@ class Simulation:
             #add the company to the companies dictionary
             self.companies[company_model.name] = self.create_company(game_model=game_model, turn_model=turn_model, company_model=company_model)
 
+        #setup the market attributes
+        market_state_model = models.MarketState.objects.get(turn=turn_model)
+        self.market = self.create_market(market_state_model=market_state_model)
+
         print(self.companies)
 
-        #TODO: setup market
-        market_state_model = models.MarketState.objects.get(turn=turn_model)
         
 
     def create_company(self, game_model : models.Game, turn_model : models.Turn, company_model : models.Company) -> Company:
@@ -58,13 +60,14 @@ class Simulation:
         except models.CompaniesState.DoesNotExist:
             company_state = None
             #TODO: add error message maybe
+
         #parse the companies state model and create objects
         if company_state != None:
 
             new_company.remaining_budget = company_state.balance    #float
             new_company.stock_price = company_state.stock_price     #float
             new_company.storage_count = company_state.inventory     #pos int
-            r_d = company_state.r_d                     #pos big int        #TODO: add rnd to class obejct
+            r_d = company_state.r_d                                 #pos big int    #TODO: add rnd to class obejct
 
             #create objects from models
             #setup factory object
@@ -109,6 +112,7 @@ class Simulation:
         new_product.set_price(production_model.sell_price)
         volume = production_model.volume #TODO add volume to product class (or maybe the company, but product makes sense)
 
+        #TODO: solve the fact that we have multiple upgrades and only one _upgrade_price in product class
         for company_upgrade_model in company_upgrades:
             upgrade_model = company_upgrade_model.upgrade
             name = upgrade_model.name #char field
