383be16855ca45c67aa54dcbeadaef2934c03a98
Lukáš Peťko <luke.petko@gmail.com>
2023-01-24 19:46:47 +0100

    fixing react structure of login

--- apps/saip-fe/src/App.tsx
+++ apps/saip-fe/src/App.tsx
@@ -16,25 +16,30 @@ import Login from "./screens/Login";
 const queryClient = new QueryClient();
 
 function App() {
+	const token = localStorage.getItem("token");
+
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Suspense>
-				<BrowserRouter>
-					<Navbar />
-					<div className="mt-16">
-						<Routes>
-							<Route path="/dashboard" element={<Dashboard />} />
-							<Route path="/product" element={<Product />} />
-							<Route path="/company" element={<Company />} />
-							<Route path="/marketing" element={<Marketing />} />
-							<Route path="/news" element={<News />} />
-							<Route path="/" element={<Navigate to="/login" replace />} />
-							<Route path="*" element={<Login />} />
-							<Route path="/login" element={<Login />} />
-						</Routes>
-					</div>
-				</BrowserRouter>
-			</Suspense>
+			{token ? (
+				<Suspense>
+					<BrowserRouter>
+						<Navbar />
+						<div className="mt-16">
+							<Routes>
+								<Route path="/dashboard" element={<Dashboard />} />
+								<Route path="/product" element={<Product />} />
+								<Route path="/company" element={<Company />} />
+								<Route path="/marketing" element={<Marketing />} />
+								<Route path="/news" element={<News />} />
+								<Route path="/" element={<Navigate to="/dashboard" replace />} />
+								<Route path="*" element={<Login />} />
+							</Routes>
+						</div>
+					</BrowserRouter>
+				</Suspense>
+			) : (
+				<Login />
+			)}
 			{/* <Devtools /> */}
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
--- apps/saip-fe/src/screens/Login.tsx
+++ apps/saip-fe/src/screens/Login.tsx
@@ -1,62 +1,90 @@
-import React from 'react';
-import { useState } from 'react';
-import { useQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState(0)
-    const [password, setPassword] = useState(0)
+	const [email, setEmail] = useState(0);
+	const [password, setPassword] = useState(0);
 
-    const login = async () => {
-        const response = await fetch('http://127.0.0.1:8000/login/', {
-            method: 'POST',
-            body: JSON.stringify({
-                'username': email,
-                'password': password,
-            }),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        })
-        return response.json();
-    }
+	const [isInvalid, setIsInvalid] = useState(false);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        //const {data, status} = useQuery('login', login)
-        login()
-        //navigate("/dashboard");
-    }
+	const login = async () => {
+		const response = await fetch("http://127.0.0.1:8000/login/", {
+			method: "POST",
+			body: JSON.stringify({
+				username: email,
+				password: password,
+			}),
+			headers: {
+				"Content-type": "application/json",
+			},
+		});
 
-    
-    return (
-        <div className="w-full  max-w-xs">
-            <form className="bg-white shadow-md rounded px-6 pt-6 pb-8 mb-4" onSubmit={ handleSubmit }>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        E-mail
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="E-mail" onChange={(e: any) => setEmail(e.target.value)}/>
-                </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        Heslo
-                    </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" onChange={(e: any) => setPassword(e.target.value)}/>
-                    <p className="text-red-500 text-xs italic">Prosím, zvoľte heslo.</p>
-                </div>
-                <div className="flex items-center justify-between">
-                <button className="bg-accent-700 hover:bg-accent-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                    Prihlásiť sa
-                </button>
-                    <a className="inline-block align-baseline font-bold text-sm text-accent-700 hover:text-accent-500" href="#">
-                        Zabudnuté heslo?
-                    </a>
-                </div>
-                
-            </form>
-            
-        </div>
-    );
+		if (response.status === 400) {
+			setIsInvalid(true);
+			return null;
+		}
+
+		const { token, expiry } = await response.clone().json();
+
+		localStorage.setItem("token", token);
+		localStorage.setItem("expiryDate", expiry);
+
+		//! temporary, find a better way to do this
+		window.location.reload();
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		//const {data, status} = useQuery('login', login)
+		login();
+		//navigate("/dashboard");
+	};
+
+	return (
+		<div className="w-full  max-w-xs">
+			<form className="bg-white shadow-md rounded px-6 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+				<div className="mb-4">
+					<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+						E-mail
+					</label>
+					<input
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						id="username"
+						type="text"
+						placeholder="E-mail"
+						onChange={(e: any) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div className="mb-6">
+					<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+						Heslo
+					</label>
+					<input
+						className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+						id="password"
+						type="password"
+						placeholder="Heslo"
+						onChange={(e: any) => setPassword(e.target.value)}
+					/>
+					<p className="text-red-500 text-xs italic">Prosím, zvoľte heslo.</p>
+				</div>
+				<div className="flex items-center justify-between">
+					<button
+						className="bg-accent-700 hover:bg-accent-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+						type="submit"
+					>
+						Prihlásiť sa
+					</button>
+					<a
+						className="inline-block align-baseline font-bold text-sm text-accent-700 hover:text-accent-500"
+						href="#"
+					>
+						Zabudnuté heslo?
+					</a>
+				</div>
+			</form>
+		</div>
+	);
 }
