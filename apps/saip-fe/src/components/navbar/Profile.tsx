b8b2a1dc284286c3a92a970c5ea103e807668cac
Lukáš Peťko <luke.petko@gmail.com>
2023-02-05 22:29:33 +0100

    adding fullscreen empty div

--- apps/saip-fe/src/components/navbar/Profile.tsx
+++ apps/saip-fe/src/components/navbar/Profile.tsx
@@ -22,18 +22,21 @@ function Profile() {
 			</button>
 
 			{open && (
-				<div className="absolute right-1 py-2 mt-2 bg-white rounded-xl shadow-xl border-accent-700 border-2">
-					<div className="flex flex-col justify-between px-4 py-2">
-						{/* TODO replace with real name */}
-						<h6 className="py-2">Company Name</h6>
-						<button
-							className="bg-accent-50 transition-all ease-in-out duration-300 rounded-lg min-w-[128px] flex justify-center align-middle bg-transparent py-2 px-3 hover:text-accent-700 hover:bg-accent-300"
-							onClick={logout}
-						>
-							Logout
-						</button>
+				<>
+					<div className="absolute right-1 py-2 mt-2 bg-white rounded-xl shadow-xl border-accent-700 border-2 z-20">
+						<div className="flex flex-col justify-between px-4 py-2">
+							{/* TODO replace with real name */}
+							<h6 className="py-2">Company Name</h6>
+							<button
+								className="bg-accent-50 transition-all ease-in-out duration-300 rounded-lg min-w-[128px] flex justify-center align-middle bg-transparent py-2 px-3 hover:text-accent-700 hover:bg-accent-300"
+								onClick={logout}
+							>
+								Logout
+							</button>
+						</div>
 					</div>
-				</div>
+					<div className="fixed inset-0 z-10" onClick={() => setOpen(false)}></div>
+				</>
 			)}
 		</>
 	);
