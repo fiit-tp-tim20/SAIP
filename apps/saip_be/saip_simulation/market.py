1c3bb6f2de5c8e96a5ac87fbf59195fb0fce5af8
vikioza <vikioza@gmail.com>
2023-02-07 14:42:50 +0100

    [BE] - resolving merge issues

--- apps/saip_be/saip_simulation/market.py
+++ apps/saip_be/saip_simulation/market.py
@@ -15,6 +15,7 @@ from .marketing import OOH
 from .config import MarketPreset
 
 
+
 @dataclass
 class Market:
     # loan_limit: int         #limit on loans (in dollars/euros)
@@ -28,18 +29,18 @@ class Market:
     customer_count: int  # overall number of potential customers
     customer_base: List[Customer]
     customer_distribution = Dict[str, int]
-
+    
     companies: List[Company]
     products: Dict[str, Product]
 
     # TODO add sensitivity to attribute(marketing, rnd, price)(maybe)
-
+    
     def __init__(self, companies, customer_count=None) -> None:
         self.companies = companies
         self.customer_base = []
         self.products = {}
         self.customer_distribution = {}
-
+        
         self.customer_count = (
             MarketPreset.STARTING_CUSTOMER_COUNT
             if customer_count is None
@@ -48,7 +49,7 @@ class Market:
         self.rnd_percentage = MarketPreset.INTERESTED_IN_RND
         self.marketing_percentage = MarketPreset.INTERESTED_IN_MARKETING
         self.price_percentage = MarketPreset.INTERESTED_IN_PRICE
-        self.__generate_product_dict()
+        self.__generate_product_dict()      
         self.generate_demand()
 
     def generate_demand(self):
@@ -59,7 +60,7 @@ class Market:
             / base_investment
             * MarketPreset.STARTING_CUSTOMER_COUNT
         )
-
+        
         for i in range(self.customer_count):
             # self.customer_base.append(Customer(self.products))  # only generating basic customer for now
             if i < self.customer_count * self.rnd_percentage:
@@ -78,19 +79,19 @@ class Market:
             ):
                 self.customer_base.append(LowBudgetustomer(self.products))
                 continue
-            self.customer_base.append(AverageBudgetCustomer(self.products))
-
+            self.customer_base.append(AverageBudgetCustomer(self.products))       
+                
     def __generate_product_dict(self):
         for company in self.companies:
             self.products[company.brand] = company.product
         self.products["no_purchase"] = LastingProduct(None, -1, -1)
-
+            
     def __get_total_investment_from_companies(self):
         total_investment = 0
         for company in self.companies:
             total_investment += company.yield_agg_marketing_value()
         return total_investment
-
+    
     def generate_distribution(self) -> Dict:
         for customer in self.customer_base:
             customer_choice = customer.choose_product()
@@ -98,7 +99,7 @@ class Market:
                 self.customer_distribution[customer_choice] = 0
             self.customer_distribution[customer_choice] += 1
         return self.customer_distribution
-
+    
 
 if __name__ == "__main__":
     comA = Company(
@@ -124,7 +125,7 @@ if __name__ == "__main__":
         {"ooh": OOH(1500)},
     )
     companies = [comA, comB, comC, comD]
-
+    
     mar = Market(companies)
     print(mar.generate_distribution())
     print(f"total = {sum(mar.customer_distribution.values())}")
