d071253dfb9386fd3e22e0cda10166f08c4f1b15
Radovan Tvrdak <radovan.tvrdak@gmai.com>
2023-02-19 10:53:39 +0100

    [BE] - Added database connection

--- apps/saip_be/saip_be/settings.py
+++ apps/saip_be/saip_be/settings.py
@@ -92,9 +92,17 @@ WSGI_APPLICATION = 'saip_be.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'saip',
+        'USER': 'saip_user',
+        'PASSWORD': 'nbusr123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -134,6 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
