76272f20b06394923605d07241cccfc0f592b33d
Lukáš Peťko <luke.petko@gmail.com>
2023-02-23 14:49:25 +0100

    adding modal to bottombar

--- apps/saip-fe/src/components/bottombar/BottomBar.tsx
+++ apps/saip-fe/src/components/bottombar/BottomBar.tsx
@@ -9,9 +9,12 @@ import useMarketingStore from "../../store/Marketing";
 import useUpgradesStore from "../../store/Upgrades";
 import { GameState } from "../../types/gameState";
 import { endTurn } from "../../api/EndTurn";
+import { useModal } from "../modal/useModal";
+import BottomBarModal from "./BottomBarModal";
 
 export default function BottomBar() {
 	const { isLoading, data } = useQuery("companyInfo", () => getGeneralInfo());
+	const { Modal, setIsShowing, setElement } = useModal(<div />);
 
 	const {
 		getSum: getSumMarketing,
@@ -53,35 +56,54 @@ export default function BottomBar() {
 		endTurn(gameState);
 	};
 
+	const handleModalSubmit = () => {
+		handleEndTurn();
+		setIsShowing(false);
+	};
+
+	const openModal = () => {
+		setElement(<BottomBarModal onAccept={handleModalSubmit} onDecline={() => setIsShowing(false)} />);
+		setIsShowing(true);
+	};
+
 	return (
-		<div className="fixed bottom-2 right-2 z-40">
-			{!isLoading ? (
-				<div className="bg-white px-3 py-1 rounded-lg border-2 border-accent-700">
-					<div className="flex flex-row gap-8 items-center">
-						<p className={`text-center font-medium ${totalSpent > data.budget_cap ? "text-red-600" : ""}`}>
-							Budget: {totalSpent}/{data.budget_cap}€
-						</p>
-						<button type="button" onClick={() => navigate("/product")} className="button-clear">
-							{/* TODO create state */}
-							Produkt: ✅
-						</button>
-						<button type="button" onClick={() => navigate("/company")} className="button-clear">
-							Spoločnosť: {getCheckedCompany() ? "✅" : "❌"}
-						</button>
-						<button type="button" onClick={() => navigate("/marketing")} className="button-clear">
-							Marketing: {getCheckedMarketing() ? "✅" : "❌"}
-						</button>
-						<button
-							type="button"
-							onClick={() => handleEndTurn()}
-							className="bg-accent-500 hover:bg-accent-700 text-white font-bold py-2 px-4 rounded-lg disabled:bg-accent-100 disabled:cursor-not-allowed"
-							disabled={totalSpent > data.budget_cap || !getCheckedCompany() || !getCheckedMarketing()}
-						>
-							Ukončiť kolo
-						</button>
+		<>
+			<Modal />
+			<div className="fixed bottom-2 right-2 z-40">
+				{!isLoading ? (
+					<div className="bg-white px-3 py-1 rounded-lg border-2 border-accent-700">
+						<div className="flex flex-row gap-8 items-center">
+							<p
+								className={`text-center font-medium ${
+									totalSpent > data.budget_cap ? "text-red-600" : ""
+								}`}
+							>
+								Budget: {totalSpent}/{data.budget_cap}€
+							</p>
+							<button type="button" onClick={() => navigate("/product")} className="button-clear">
+								{/* TODO create state */}
+								Produkt: ✅
+							</button>
+							<button type="button" onClick={() => navigate("/company")} className="button-clear">
+								Spoločnosť: {getCheckedCompany() ? "✅" : "❌"}
+							</button>
+							<button type="button" onClick={() => navigate("/marketing")} className="button-clear">
+								Marketing: {getCheckedMarketing() ? "✅" : "❌"}
+							</button>
+							<button
+								type="button"
+								onClick={openModal}
+								className="bg-accent-500 hover:bg-accent-700 text-white font-bold py-2 px-4 rounded-lg disabled:bg-accent-100 disabled:cursor-not-allowed"
+								disabled={
+									totalSpent > data.budget_cap || !getCheckedCompany() || !getCheckedMarketing()
+								}
+							>
+								Ukončiť kolo
+							</button>
+						</div>
 					</div>
-				</div>
-			) : null}
-		</div>
+				) : null}
+			</div>
+		</>
 	);
 }
