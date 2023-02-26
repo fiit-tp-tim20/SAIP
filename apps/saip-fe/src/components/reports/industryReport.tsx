646faa6a3acbbe438abbcf8effa6b27a10e5baf3
TomasLetanovsky <tomas.letanovsky@gmail.com>
2023-02-26 14:39:00 +0100

    Dashboard rozdelený na company a industry reporty

---
+++ apps/saip-fe/src/components/reports/industryReport.tsx
@@ -0,0 +1,13 @@
+import { useTranslation } from "react-i18next";
+
+function IndustryReport() {
+	const { t } = useTranslation();
+
+    return (
+        <div>
+            Industry report goes here
+        </div>
+        );
+    }
+    
+export default IndustryReport;
--- apps/saip-fe/src/screens/Dashboard.tsx
+++ apps/saip-fe/src/screens/Dashboard.tsx
@@ -1,198 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import CompanyReport from "../components/reports/companyReport";
+import IndustryReport from "../components/reports/industryReport";
+
 
 function Plan() {
 	const { t } = useTranslation();
 
+	const [showCompanyReport, setShowCompanyReport] = useState(true);
+
+	const handleSwitchScreen = () => {
+		setShowCompanyReport(!showCompanyReport);
+	};
+
 	return (
-		// TODO - breakpoint values
+		<div>
+		{showCompanyReport ? 
+		(<button className="bg-accent-500 hover:bg-accent-700 text-white font-bold py-2 px-4 m-0 rounded-lg" onClick={handleSwitchScreen}>Prejsť na správu o trhu</button>) :
+		(<button className="bg-accent-500 hover:bg-accent-700 text-white font-bold py-2 px-4 m-0 rounded-lg" onClick={handleSwitchScreen}>Prejsť na správu o spoločnosti</button>) }
+		{showCompanyReport ? <CompanyReport /> : <IndustryReport />}
 
-		<div className="flex w-[600px] flex-col md:w-[900px] xl:w-[1280px]">
-				<h1 className="my-4">Správa o spoločnosti</h1>
-				<div className="grid gap-4 xl:grid-cols-3">
-					<div className="background-container my-2 flex flex-col rounded-2xl p-6">
-						<div className="flex flex-row items-center justify-between py-2">
-							<h2>Produckia</h2>
-						</div>			
-							<table className="table-auto">
-								<tbody>
-									<tr>
-									<td className="border px-4 py-2">Produkcia</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Výrobná kapacita</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Utilizácia</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Výrobná cena</td>
-									<td className="border px-4 py-2">100€</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Inventár</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-								</tbody>
-								</table>
-					</div>
-					<div className="background-container my-2 flex flex-col rounded-2xl p-6">
-						<div className="flex flex-row items-center justify-between py-2">
-							<h2>Správa o predaji</h2>
-						</div>			
-							<table className="table-auto">
-								<tbody>
-									<tr>
-									<td className="border px-4 py-2">Prijaté objednávky</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Splnené objednávky</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Nesplnené objednávky</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Predajná cena</td>
-									<td className="border px-4 py-2">100€</td>
-									</tr>
-								</tbody>
-							</table>
-					</div>
-					<div className="background-container my-2 flex flex-col rounded-2xl p-6">
-						<div className="flex flex-row items-center justify-between py-2">
-							<h2>Súvaha</h2>
-						</div>			
-							<table className="table-auto">
-								<thead>
-									<tr>
-									<th className="px-4 py-2">Aktíva</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-									<td className="border px-4 py-2">Hotovosť</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Zásoby</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Kapitálové invsetície</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-								</tbody>
-								<thead>
-									<tr>
-									<th className="px-4 py-2">Pasíva</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-									<td className="border px-4 py-2">Pôžičky</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Nerozdelený zisk</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Vlastné ímanie</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-								</tbody>
-							</table>
-					</div>
-					<div className="background-container my-2 flex flex-col rounded-2xl p-6">
-						<div className="flex flex-row items-center justify-between py-2">
-							<h2>Cashflow</h2>
-						</div>			
-							<table className="table-auto">
-								{/* <thead>
-									<tr>
-									<th className="px-4 py-2">Column 1</th>
-									<th className="px-4 py-2">Column 2</th>
-									</tr>
-								</thead> */}
-								<tbody>
-									<tr>
-									<td className="border px-4 py-2">Počiatočná hotovosť</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Kapitálové invstície</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Čistý zisk</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Odpisy</td>
-									<td className="border px-4 py-2">100€</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Poplatok za zásoby</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Nové úvery</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-								</tbody>
-							</table>
-					</div>
-					<div className="background-container my-2 flex flex-col rounded-2xl p-6">
-						<div className="flex flex-row items-center justify-between py-2">
-							<h2>Výkaz ziskov a strát</h2>
-						</div>			
-							<table className="table-auto">
-								<tbody>
-									<tr>
-									<td className="border px-4 py-2">Tržby</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Náklady na predajný tovar</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Marketing</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Poplatok za zásoby</td>
-									<td className="border px-4 py-2">100€</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Úrok</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Zisk pred zdanením</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Daň</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-									<tr>
-									<td className="border px-4 py-2">Čistý zisk</td>
-									<td className="border px-4 py-2">100</td>
-									</tr>
-								</tbody>
-							</table>
-					</div>
-
-				</div>
 		</div>
 	);
+	
 }
 
 export default Plan;
