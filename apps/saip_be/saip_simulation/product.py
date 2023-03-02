b58cbf08f867098d3de2d4f15eb8d77f98986675
vikioza <vikioza@gmail.com>
2023-03-02 13:05:31 +0100

    [BE] - product dataclass bugfix

--- apps/saip_be/saip_simulation/product.py
+++ apps/saip_be/saip_simulation/product.py
@@ -10,24 +10,23 @@ class Upgrade:
     effect: float  # modifier for expected value increase
 
 
-
 @dataclass
 class Product(ABC):
     upgrades: dict = None
-    __price: float = 0
-    __upgrade_price: float = 0
+    _price: float = 0
+    _upgrade_price: float = 0
 
     def set_price(self, new_price: float) -> None:
-        self.__price = new_price
+        self._price = new_price
 
     def get_price(self) -> float:
-        return self.__price
+        return self._price
 
     def _set_upgrade_price(self, new_upgrade_price: float) -> None:
         self._set_upgrade_price = new_upgrade_price
 
     def get_upgrade_price(self) -> float:
-        return self.__upgrade_price
+        return self._upgrade_price
 
     def upgrade_stored_products(self):
         self._perform_upgrade_logic()
@@ -52,7 +51,7 @@ class LastingProduct(Product):
     # ToDo replace dummy logic (used in testing) witch actual code
 
     def _calculate_upgrade_price(self) -> float:
-        return self.__price / 10
+        return self._price / 10
 
     def _perform_upgrade_logic(self) -> None:
-        self.__price = self.__price * 1.1
+        self._price = self._price * 1.1
