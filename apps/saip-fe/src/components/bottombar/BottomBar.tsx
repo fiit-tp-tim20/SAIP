76b8d281b3c847ebdd566046529589f25675186f
Lukáš Peťko <luke.petko@gmail.com>
2023-03-09 17:40:05 +0100

    adding commited message

--- apps/saip-fe/src/components/bottombar/BottomBar.tsx
+++ apps/saip-fe/src/components/bottombar/BottomBar.tsx
@@ -76,38 +76,42 @@ export default function BottomBar() {
 			<div className="fixed bottom-2 right-2 z-40">
 				{!isLoading ? (
 					<div className="bg-white px-3 py-1 rounded-lg border-2 border-accent-700">
-						<div className="flex flex-row gap-8 items-center">
-							<p
-								className={`text-center font-medium ${
-									totalSpent > data.budget_cap ? "text-red-600" : ""
-								}`}
-							>
-								Budget: {totalSpent}/{data.budget_cap}€
-							</p>
-							<button type="button" onClick={() => navigate("/product")} className="button-clear">
-								{/* TODO create state */}
-								Produkt: ✅
-							</button>
-							<button type="button" onClick={() => navigate("/company")} className="button-clear">
-								Spoločnosť: {getCheckedCompany() ? "✅" : "❌"}
-							</button>
-							<button type="button" onClick={() => navigate("/marketing")} className="button-clear">
-								Marketing: {getCheckedMarketing() ? "✅" : "❌"}
-							</button>
-							<button
-								type="button"
-								onClick={openModal}
-								className="bg-accent-500 hover:bg-accent-700 text-white font-bold py-2 px-4 rounded-lg disabled:bg-accent-100 disabled:cursor-not-allowed"
-								disabled={
-									totalSpent > data.budget_cap ||
-									!getCheckedCompany() ||
-									!getCheckedMarketing() ||
-									commited
-								}
-							>
-								Ukončiť kolo
-							</button>
-						</div>
+						{commited ? (
+							<p className="text-center font-medium p-3">Čaká sa na ostatných hráčov</p>
+						) : (
+							<div className="flex flex-row gap-8 items-center">
+								<p
+									className={`text-center font-medium ${
+										totalSpent > data.budget_cap ? "text-red-600" : ""
+									}`}
+								>
+									Budget: {totalSpent}/{data.budget_cap}€
+								</p>
+								<button type="button" onClick={() => navigate("/product")} className="button-clear">
+									{/* TODO create state */}
+									Produkt: ✅
+								</button>
+								<button type="button" onClick={() => navigate("/company")} className="button-clear">
+									Spoločnosť: {getCheckedCompany() ? "✅" : "❌"}
+								</button>
+								<button type="button" onClick={() => navigate("/marketing")} className="button-clear">
+									Marketing: {getCheckedMarketing() ? "✅" : "❌"}
+								</button>
+								<button
+									type="button"
+									onClick={openModal}
+									className="bg-accent-500 hover:bg-accent-700 text-white font-bold py-2 px-4 rounded-lg disabled:bg-accent-100 disabled:cursor-not-allowed"
+									disabled={
+										totalSpent > data.budget_cap ||
+										!getCheckedCompany() ||
+										!getCheckedMarketing() ||
+										commited
+									}
+								>
+									Ukončiť kolo
+								</button>
+							</div>
+						)}
 					</div>
 				) : null}
 			</div>
