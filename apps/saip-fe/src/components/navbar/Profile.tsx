f6a26c24b7be0f6f3c86b3c629871c77c5f320a4
Lukáš Peťko <luke.petko@gmail.com>
2023-03-09 17:36:21 +0100

    fixing data reset on logout for upgrades

--- apps/saip-fe/src/components/navbar/Profile.tsx
+++ apps/saip-fe/src/components/navbar/Profile.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "react-query";
 import { getGeneralInfo } from "../../api/CompanyInfo";
 import useCompanyStore from "../../store/Company";
 import useMarketingStore from "../../store/Marketing";
+import useUpgradesStore from "../../store/Upgrades";
 
 function Profile() {
 	const [open, setOpen] = useState(false);
@@ -11,6 +12,7 @@ function Profile() {
 
 	const { reset: marketingReset } = useMarketingStore();
 	const { reset: companyReset } = useCompanyStore();
+	const { reset: upgradesReset } = useUpgradesStore();
 
 	//! just for testing, find a better place to do this
 	const logout = async () => {
@@ -27,46 +29,44 @@ function Profile() {
 		localStorage.removeItem("expiryDate");
 		marketingReset();
 		companyReset();
+		upgradesReset();
 		window.location.reload();
 	};
 
-	return (
+	return !isLoading ? (
 		<>
-			{!isLoading ? (
-				<>
-					<button
-						className="p-0 border-4 border-accent-400 ring-offset-base-100 ring-offset-2 w-10 h-10 rounded-full bg-gray-200"
-						type="button"
-						onClick={() => setOpen(!open)}
-					>
-						{/* TODO replace with real avatar */}
-						<img
-							className="rounded-full"
-							src={`https://avatars.dicebear.com/api/miniavs/${data.id}a.png`}
-							alt="user photo"
-						/>
-					</button>
+			<button
+				className="p-0 border-4 border-accent-400 ring-offset-base-100 ring-offset-2 w-10 h-10 rounded-full bg-gray-200"
+				type="button"
+				onClick={() => setOpen(!open)}
+			>
+				{/* TODO replace with real avatar */}
+				<img
+					className="rounded-full"
+					src={`https://avatars.dicebear.com/api/miniavs/${data.id}a.png`}
+					alt="user profile "
+				/>
+			</button>
 
-					{open && (
-						<>
-							<div className="absolute right-6 py-2 mt-4 bg-white rounded-xl shadow-xl border-accent-700 border-2 z-20">
-								<div className="flex flex-col justify-between px-4 py-2">
-									<h6 className="py-2">{data.name}</h6>
-									<button
-										className="bg-accent-50 transition-all ease-in-out duration-300 rounded-lg min-w-[128px] flex justify-center align-middle bg-transparent py-2 px-3 hover:text-accent-700 hover:bg-accent-300"
-										onClick={logout}
-									>
-										Logout
-									</button>
-								</div>
-							</div>
-							<div className="fixed inset-0 z-10" onClick={() => setOpen(false)}></div>
-						</>
-					)}
+			{open && (
+				<>
+					<div className="absolute right-6 py-2 mt-4 bg-white rounded-xl shadow-xl border-accent-700 border-2 z-20">
+						<div className="flex flex-col justify-between px-4 py-2">
+							<h6 className="py-2">{data.name}</h6>
+							<button
+								type="button"
+								className="bg-accent-50 transition-all ease-in-out duration-300 rounded-lg min-w-[128px] flex justify-center align-middle bg-transparent py-2 px-3 hover:text-accent-700 hover:bg-accent-300"
+								onClick={logout}
+							>
+								Logout
+							</button>
+						</div>
+					</div>
+					<div className="fixed inset-0 z-10" onClick={() => setOpen(false)} />
 				</>
-			) : null}
+			)}
 		</>
-	);
+	) : null;
 }
 
 export default Profile;
