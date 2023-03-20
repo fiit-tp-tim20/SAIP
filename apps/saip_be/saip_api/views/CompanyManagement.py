78d1f2f12b057841eb872743c58752249912bf50
Nikola Blahovičová <niki.blahovicova@gmail.com>
2023-03-20 15:05:18 +0100

    oprava chyby

--- apps/saip_be/saip_api/views/CompanyManagement.py
+++ apps/saip_be/saip_api/views/CompanyManagement.py
@@ -143,7 +143,7 @@ class CompanyReport(APIView):
         balance['liabilities_summary'] = company_state_previous.loans + company_state_previous.ret_earnings + company.game.parameters.base_capital
 
         cash_flow = dict()
-        cash_flow['beginning_cash'] = CompaniesState.objects.get(turn=Turn.objects.get(number=last_turn.number-1), company=company).cash #???
+        cash_flow['beginning_cash'] = CompaniesState.objects.get(turn=Turn.objects.get(game=company.game, number=last_turn.number-1), company=company).cash #???
         cash_flow['sales'] =  company_state_previous.sales #plus
         cash_flow['manufactured_man_cost'] = company_state_previous.manufactured_man_cost #minus
         # vydavky na rozhodnutia - zratane vydavky na marketing r_d a capital s minusovou hodnotou
