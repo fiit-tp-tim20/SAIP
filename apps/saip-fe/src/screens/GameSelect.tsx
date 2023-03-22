7878404bfb9c54556cd5eb8b31c6e705328c636a
Marek Oravec <marekoravec4@gmail.com>
2023-03-22 21:26:59 +0100

    [FE] - added GET to list the games

--- apps/saip-fe/src/screens/GameSelect.tsx
+++ apps/saip-fe/src/screens/GameSelect.tsx
@@ -1,181 +1,192 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-const test_games = [{
-    "id": 69,
-    "name": "Kantrencik-LS22-UT-1300"
-  },{
-    "id": 70,
-    "name": "Zatrochova-LS22-UT-1300"
-  }]
-
-  
+const test_games = [
+	{
+		id: 69,
+		name: "Kantrencik-LS22-UT-1300",
+	},
+	{
+		id: 70,
+		name: "Zatrochova-LS22-UT-1300",
+	},
+];
 
 type User = {
-    name: string;
-    id: number;
-}
+	name: string;
+	id: number;
+};
 
+export default function GameSelect() {
+	const [games, setGames] = useState([]);
+	const [users, setUsers] = useState<User[]>([]);
+	const [inputNumbersOfUsers, setInputNumbersOfUsers] = useState(0);
+	const [companyName, setCompanyName] = useState("");
 
-
-function GameSelect() {
-
-
-    const [games, setGames] = useState(test_games);
-    const [users, setUsers] = useState<User[]>([]);
-    const [inputNumbersOfUsers,setInputNumbersOfUsers] = useState(0);
-    const [companyName, setCompanyName] = useState('');
-
-
-    
-    
-    const confirm = async () => {
+	const confirm = async () => {
 		const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/list_games/`, {
-            //treba pridat bearer token
+			//treba pridat bearer token
 			method: "GET",
 			headers: {
 				"Content-type": "application/json",
 			},
-            // headers: {
-            //     "Content-Type": "application/json",
-            //     Authorization: `Bearer ${token}`,
-            // },
+			// headers: {
+			//     "Content-Type": "application/json",
+			//     Authorization: `Bearer ${token}`,
+			// },
 		});
-        console.log(response);
-            
-    }
+		console.log(response);
+	};
 
-    const setPlayers = async() => {
-        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/list_games/`, {
-            //treba pridat bearer token
+	useEffect(() => {
+		fetch(`${import.meta.env.VITE_BACKEND_URL}/list_games/`)
+			.then((response) => response.json())
+			.then((data) => {
+				setGames(data);
+			})
+			.catch((error) => {
+				console.error("Error fetching data:", error);
+			});
+	}, []);
+
+	const setPlayers = async () => {
+		const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/list_games/`, {
+			//treba pridat bearer token
 			method: "POST",
 			headers: {
 				"Content-type": "application/json",
 			},
-            // headers: {
-            //     "Content-Type": "application/json",
-            //     Authorization: `Bearer ${token}`,
-            // },
-		}); 
-    }
+			// headers: {
+			//     "Content-Type": "application/json",
+			//     Authorization: `Bearer ${token}`,
+			// },
+		});
+	};
 
+	function setNumberOfUsers(numOfPlayers: number) {
+		const obj = [...Array(numOfPlayers).keys()].map((number) => {
+			return { value: "", id: number };
+		});
+		setInputNumbersOfUsers(numOfPlayers);
+		setUsers(obj);
+	}
 
-
-    function setNumberOfUsers(numOfPlayers : number) {
-        const obj = [...Array(numOfPlayers).keys()].map((number) => {
-            return {value:"",id:number}
-        })
-        setInputNumbersOfUsers(numOfPlayers)
-        setUsers(obj);
-
-    }
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		//const {data, status} = useQuery('login', login)
 		// confirm();
-        const arrayOfNames = users.map(user => user.value);
-        console.log(arrayOfNames);
-        fetch(`${import.meta.env.VITE_BACKEND_URL}/create_company/`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                game: games,
-                name: companyName,
-                participants: arrayOfNames,
-            })
-        })
-            .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch((error) => console.error(error));
-  };
-        
-        //setPlayers();
+		const arrayOfNames = users.map((user) => user.value);
+		console.log(arrayOfNames);
+		fetch(`${import.meta.env.VITE_BACKEND_URL}/create_company/`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("token")}`,
+			},
+			body: JSON.stringify({
+				game: games,
+				name: companyName,
+				participants: arrayOfNames,
+			}),
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data))
+			.catch((error) => console.error(error));
+
+		//setPlayers();
 		//navigate("/dashboard");
 	};
 
+	const updateUser = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+		setUsers((users) =>
+			users.map((user) => {
+				if (user.id === id) {
+					return { ...user, value: e.target.value };
+				}
+				return user;
+			}),
+		);
+	};
 
-    const updateUser = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
-        setUsers((users) =>
-          users.map((user) => {
-            if (user.id === id) {
-              return { ...user, value: e.target.value };
-            }
-            return user;
-          })
-        );
-      };
-
-    
-
-    return (
-        <div className="w-full  max-w-xs">
-            <form className="bg-white shadow-md rounded px-6 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        Názov tímu
-                    </label>
-                    <input
+	return (
+		<div className="w-full  max-w-xs">
+			<form className="bg-white shadow-md rounded px-6 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+				<div className="mb-6">
+					<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+						Názov tímu
+					</label>
+					<input
 						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 						id="username"
 						type="text"
 						placeholder="Názov vašej spoločnosti"
-                        onChange={(e: any) => setCompanyName(e.target.value)}
+						onChange={(e: any) => setCompanyName(e.target.value)}
 					/>
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Zvoľte vaše cvičenie</label>
-                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-accent-500 focus:border-accent-500 block w-full p-2.5">
-                       {games.map(({id,name}) => 
-                        <>
-                        <option value={name}>{name}</option>
-                        </>
-                       )}
-                    </select>
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Počet ľudí v tíme</label>
-                    <select onChange={e => setNumberOfUsers(+e.target.value)}  value={inputNumbersOfUsers}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-accent-500 focus:border-accent-500 block w-full p-2.5">
-                        <option key="0" value="0">0</option>
-                        <option key="1" value="1">1</option>
-                        <option key="2" value="2">2</option>
-                        <option key="3" value="3">3</option>
-                        <option key="4" value="4">4</option>
-                        <option key="5" value="5">5</option>
-                        <option key="6" value="6">6</option>
-                    
-                    </select>
-                </div>
-                {users?.length > 0 && ( 
-                <div className="mb-6">
-                    {users.map(({value,id}) => (
-                    <div>
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                            Celé meno
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="username"
-                            type="text"
-                            placeholder="Celé meno"
-                            value={value}
-                            onChange={e => updateUser(e,+id)}
-                        />
-                    </div>
-          ))}
-                </div>
-            )}
-            <div className="flex items-center justify-between">
-                <button
-                    className="w-full bg-accent-700 hover:bg-accent-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="submit"
-                    >
-                    Potvrdiť
-                </button>
-            </div>
-            </form>
-        </div>
-    );
-    
+					<label className="block text-gray-700 text-sm font-bold mb-2">Zvoľte vaše cvičenie</label>
+					<select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-accent-500 focus:border-accent-500 block w-full p-2.5">
+						{games.map(({ id, name }) => (
+							<>
+								<option value={name}>{name}</option>
+							</>
+						))}
+					</select>
+					<label className="block text-gray-700 text-sm font-bold mb-2">Počet ľudí v tíme</label>
+					<select
+						onChange={(e) => setNumberOfUsers(+e.target.value)}
+						value={inputNumbersOfUsers}
+						className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-accent-500 focus:border-accent-500 block w-full p-2.5"
+					>
+						<option key="0" value="0">
+							0
+						</option>
+						<option key="1" value="1">
+							1
+						</option>
+						<option key="2" value="2">
+							2
+						</option>
+						<option key="3" value="3">
+							3
+						</option>
+						<option key="4" value="4">
+							4
+						</option>
+						<option key="5" value="5">
+							5
+						</option>
+						<option key="6" value="6">
+							6
+						</option>
+					</select>
+				</div>
+				{users?.length > 0 && (
+					<div className="mb-6">
+						{users.map(({ value, id }) => (
+							<div>
+								<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+									Celé meno
+								</label>
+								<input
+									className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+									id="username"
+									type="text"
+									placeholder="Celé meno"
+									value={value}
+									onChange={(e) => updateUser(e, +id)}
+								/>
+							</div>
+						))}
+					</div>
+				)}
+				<div className="flex items-center justify-between">
+					<button
+						className="w-full bg-accent-700 hover:bg-accent-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+						type="submit"
+					>
+						Potvrdiť
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
-
-export default GameSelect;
