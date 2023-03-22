d78db8db10fb3d2f9ea6fbfe39d9286cd1eb7f8e
Lukáš Peťko <luke.petko@gmail.com>
2023-03-22 11:05:02 +0100

    removing unused variable

--- apps/saip-fe/src/components/navbar/Profile.tsx
+++ apps/saip-fe/src/components/navbar/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import getGeneralInfo from "../../api/CompanyInfo";
 import logout from "../../api/logout";
