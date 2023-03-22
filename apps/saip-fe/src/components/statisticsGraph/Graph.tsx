e2190e324136f2a8413586c2b8b34485919b7d37
TomasLetanovsky <tomas.letanovsky@gmail.com>
2023-03-22 14:16:08 +0100

    Linegraph base not working yet

--- apps/saip-fe/src/components/statisticsGraph/Graph.tsx
+++ apps/saip-fe/src/components/statisticsGraph/Graph.tsx
@@ -1,50 +1,34 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "react-query";
-import { Line } from 'react-chartjs-2';
+import { Line } from "react-chartjs-2";
+import { X } from "react-feather";
 
 type data = {
-    data: number[];
-}
-
-
+	data: number[];
+};
 
 const LineGraph = ({ data }: data) => {
-    console.log(data);
-    const chartData = {
-      labels: Array.from({ length: data.length }, (_, i) => i + 1),
-      datasets: [
-        {
-          label: 'Data',
-          data,
-          fill: false,
-          borderColor: 'rgba(75,192,192,1)',
-          tension: 0.1
-        }
-      ]
-    };
-    
-    const chartOptions = {
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'Data Point'
-          }
-        },
-        y: {
-          title: {
-            display: true,
-            text: 'Value'
-          },
-          suggestedMin: 0
-        }
-      }
-    };
-  
-    return (
-      <Line data={chartData} options={chartOptions} />
-    );
-  };
-  
-  export default LineGraph;
+    console.log(data)
+	const chartData = {
+		labels: Array.from({ length: data.length }, (_, i) => i + 1),
+		datasets: [
+			{
+				label: "Demand",
+				data: data,
+				fill: false,
+				borderColor: "rgb(75, 192, 192)",
+				tension: 0.1
+			},
+		],
+	};
+
+
+
+
+
+
+	return <Line data={chartData} />;
+};
+
+export default LineGraph;
