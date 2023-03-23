96c980d462c77f2c6928d79717863846dff9b5ce
TomasLetanovsky <tomas.letanovsky@gmail.com>
2023-03-23 12:10:47 +0100

    Changed to react-charts not-working yet

--- apps/saip-fe/src/components/statisticsGraph/Graph.tsx
+++ apps/saip-fe/src/components/statisticsGraph/Graph.tsx
@@ -1,34 +1,36 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "react-query";
-import { Line } from "react-chartjs-2";
-import { X } from "react-feather";
+import { Chart } from "react-charts";
 
-type data = {
+type LineGraphProps = {
 	data: number[];
 };
 
-const LineGraph = ({ data }: data) => {
-    console.log(data)
-	const chartData = {
-		labels: Array.from({ length: data.length }, (_, i) => i + 1),
-		datasets: [
-			{
-				label: "Demand",
-				data: data,
-				fill: false,
-				borderColor: "rgb(75, 192, 192)",
-				tension: 0.1
-			},
-		],
-	};
+function LineGraph({ data: data }: LineGraphProps) {
+  const chartData = React.useMemo(
+    () => [
+      {
+        label: 'Values',
+        data: data.map((value, index) => [index, value]),
+      },
+    ],
+    [data]
+  );
 
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'linear', position: 'bottom' },
+      { type: 'linear', position: 'left' },
+    ],
+    []
+  );
 
-
-
-
-
-	return <Line data={chartData} />;
-};
+  return (
+    <div style={{ width: '400px', height: '300px' }}>
+      <Chart data={chartData} axes={axes} />
+    </div>
+  );
+}
 
 export default LineGraph;
