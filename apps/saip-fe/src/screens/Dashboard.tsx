fbd47aa1c504857e73a6e5462aed5a72d3005bcc
Lukáš Peťko <luke.petko@gmail.com>
2023-03-24 22:05:38 +0100

    adding button group on top of dashboard

--- apps/saip-fe/src/screens/Dashboard.tsx
+++ apps/saip-fe/src/screens/Dashboard.tsx
@@ -1,33 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { getTurn } from "../api/GetTurn";
 import CompanyReport from "../components/reports/companyReport";
 import IndustryReport from "../components/reports/industryReport";
 
 function Plan() {
 	const [showCompanyReport, setShowCompanyReport] = useState(true);
+	const { data: currentTurn } = useQuery("currentTurn", getTurn);
 
-	const handleSwitchScreen = () => {
-		setShowCompanyReport(!showCompanyReport);
-	};
+	useEffect(() => {
+		if (currentTurn.Number === 1) {
+			setShowCompanyReport(true);
+		}
+	}, [currentTurn]);
 
 	return (
-		<div>
-			{showCompanyReport ? (
+		<div className="flex flex-col items-center">
+			<div className="inline-flex rounded-md shadow-sm pt-4" role="group">
 				<button
-					className="bg-accent-500 hover:bg-accent-700 text-white font-bold py-2 px-4 m-0 rounded-lg"
 					type="button"
-					onClick={handleSwitchScreen}
+					className={`hover:bg-accent-700 font-bold py-2 px-4 m-0 rounded-l-lg transition-all duration-300 ${
+						showCompanyReport
+							? "bg-accent-500 text-white border-none hover:bg-accent-700"
+							: "bg-white text-accent-500 border-accent-500 border hover:bg-accent-300 hover:text-white hover:border-accent-300"
+					}`}
+					onClick={() => setShowCompanyReport(true)}
 				>
-					Prejsť na správu o trhu
+					Správa o spoločnosti
 				</button>
-			) : (
 				<button
-					className="bg-accent-500 hover:bg-accent-700 text-white font-bold py-2 px-4 m-0 rounded-lg"
 					type="button"
-					onClick={handleSwitchScreen}
+					className={`hover:bg-accent-700 font-bold py-2 px-4 m-0 rounded-r-lg transition-all duration-300 disabled:text-gray-300 disabled:border-gray-300 disabled:hover:bg-white ${
+						!showCompanyReport
+							? "bg-accent-500 text-white border-none hover:bg-accent-700"
+							: "bg-white text-accent-500 border-accent-500 border hover:bg-accent-300 hover:text-white hover:border-accent-300"
+					}
+                    `}
+					onClick={() => setShowCompanyReport(false)}
+					disabled={currentTurn.Number === 1}
 				>
-					Prejsť na správu o spoločnosti
+					Správa o trhu
 				</button>
-			)}
+			</div>
+
 			{showCompanyReport ? <CompanyReport /> : <IndustryReport />}
 		</div>
 	);
