856d5caa9a627d5a31be5395bc63af69bb4c757c
Radovan Tvrdak <radovan.tvrdak@gmai.com>
2023-03-31 19:41:40 +0200

    [BE] - Updated logging.

--- apps/saip_be/saip_be/settings.py
+++ apps/saip_be/saip_be/settings.py
@@ -185,7 +185,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(message)s'
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
@@ -196,6 +196,7 @@ LOGGING = {
             'level': 'INFO',
             'class': 'logging.FileHandler',
             'filename': LOG_FILE,
+            'formatter': 'verbose'
         },
         'console': {
             'level': 'ERROR',
