9670f845acd07c6dc5e7644a1e510e25be641a51
Jakub Krkoska <kuberto773@github.com>
2023-11-16 15:55:35 +0100

    [BE] - added POC method for notifying sers

--- apps/saip_be/saip_ws/consumers.py
+++ apps/saip_be/saip_ws/consumers.py
@@ -2,15 +2,20 @@ import json
 
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 from channels.auth import login
+from asgiref.sync import async_to_sync
 
 class TestConsumer(WebsocketConsumer):
     def connect(self):
         self.user = self.scope["user"]
         if not self.user or not self.user.is_authenticated:
             return Response({"detail": "User is not authenticated"}, status=401)
+        async_to_sync(self.channel_layer.group_add)("game", self.channel_name)
         self.accept()
         self.send(text_data="Websocket connected")
-        
+
+    def game_notify(self, event):
+        print("**********NOTIFIED**********")
+        self.send(text_data=event["text"])        
 
     def disconnect(self, close_code):
         pass
