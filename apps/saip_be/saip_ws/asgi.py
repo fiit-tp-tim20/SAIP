70d30edbb56b096f369a7cf100c74ac5445a6200
Jakub Krkoska <kuberto773@github.com>
2023-11-23 20:18:33 +0100

    [BE] - changed order of  imports for asgi.py

--- apps/saip_be/saip_ws/asgi.py
+++ apps/saip_be/saip_ws/asgi.py
@@ -1,22 +1,26 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saip_be.settings")
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
+
 from . import routing
 from . import custom_auth
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-django_asgi_app = get_asgi_application()
 
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            custom_auth.TokenAuthMiddleware(URLRouter(routing.websocket_urlpatterns))
+            #custom_auth.TokenAuthMiddleware
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
         ),
     }
 )
