949b0ed6f32d2a54e3e225f0d6a265576b8ea59e
Jakub Krkoska <kuberto773@github.com>
2024-02-05 23:40:39 +0100

    [FE] - changed ws url

--- apps/saip-fe/src/App.tsx
+++ apps/saip-fe/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 	const [data, setData] = useState(null);
 	useEffect(() => {
 		// @ts-ignore
-		const chatSocket = new WebSocket("ws://team23-23.studenti.fiit.stuba.sk/ws/turn_info/", ["token", token]);
+		const chatSocket = new WebSocket("wss://team23-23.studenti.fiit.stuba.sk/ws/turn_info/", ["token", token]);
 		chatSocket.onmessage = function (e) {
 			// @ts-ignore
 			console.log(e.data);
