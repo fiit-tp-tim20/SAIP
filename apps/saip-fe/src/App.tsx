0b81d383f952465d330ae1f6acb146af80619819
lubomirsilny <lubomir.silny.01@gmail.com>
2024-03-23 12:15:48 +0100

    fix ws uz funguje

--- apps/saip-fe/src/App.tsx
+++ apps/saip-fe/src/App.tsx
@@ -1,4 +1,5 @@
 import React, {createContext, Suspense, useContext, useEffect, useState} from "react";
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 import "./App.css";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 // @ts-ignore
@@ -35,17 +36,22 @@ function App() {
 		comm: null,
 		start:  null
 	});
-	useEffect(() => {
-		// @ts-ignore
-		const chatSocket = new WebSocket(`${import.meta.env.VITE_WS_URL}turn_info/`, ['token', token]);
-		chatSocket.onmessage = function (e) {
+	const {   sendMessage,
+		sendJsonMessage,
+		lastMessage,
+		lastJsonMessage,
+		readyState,
+		getWebSocket,} = useWebSocket(`${import.meta.env.VITE_WS_URL}turn_info/`, {
+		protocols: ['authorization', `${token}`],
+		onOpen: () => console.log('opened'),
+		onClose: () => console.log('closed'),
+		onMessage: (e) =>{
+			console.log(e.data)
 			// @ts-ignore
-			console.log(e.data);
-			console.log(connect)
 			if (e.data === 'Websocket connected') {
 				setConnect('yes');
 			}
-			if (connect === 'yes' && e.data[0] === '{'){
+			if (e.data[0] === '{' && connect == 'yes') {
 				try {
 					const receivedData = JSON.parse(e.data);
 					setData({
@@ -58,19 +64,12 @@ function App() {
 				}
 			}
 
-		};
-		chatSocket.onclose = function (e) {
-			console.log(e)
-			console.error('Chat socket closed unexpectedly');
-		};
-
-		// Cleanup function
-		return () => {
-			chatSocket.close();
-		};
-
-		// eslint-disable-next-line
-	}, [token, connect, data.num]);
+		},
+		share: true,
+		shouldReconnect: (closeEvent) => true,
+		reconnectAttempts: 10,
+		reconnectInterval: 30000,
+	});
 	const { reset: resetCompanyState } = useCompanyStore();
 	const { reset: resetUpgradeState } = useUpgradesStore();
 	const { reset: resetMarketingState } = useMarketingStore();
