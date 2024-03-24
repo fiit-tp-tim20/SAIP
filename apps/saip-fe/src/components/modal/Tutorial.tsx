262b33710f29d48973eccf62ce2275489f3e8dd9
Filip Mačina <71822287+Maci7772@users.noreply.github.com>
2024-03-24 18:36:07 +0100

    FE - tutorial fix 2

--- apps/saip-fe/src/components/modal/Tutorial.tsx
+++ apps/saip-fe/src/components/modal/Tutorial.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 // @ts-ignore
 import Modal from "react-modal";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
-const { t } = useTranslation();
+// const { t } = useTranslation();
 
 // @ts-ignore
 function Tutorial({ isOpen, closeModal, textTitle, textContent }) {
