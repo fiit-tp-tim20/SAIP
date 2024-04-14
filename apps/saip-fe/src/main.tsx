3dea30c1c2a5d8037c1cf52c42fb63c88beafa96
lubomirsilny <lubomir.silny.01@gmail.com>
2024-04-14 19:28:44 +0200

    build fix

--- apps/saip-fe/src/main.tsx
+++ apps/saip-fe/src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
-import App from "./App";
+// @ts-ignore
+import App from "./App.tsx";
 import "./index.css";
 
 const queryClient = new QueryClient();
 
+// @ts-ignore
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<QueryClientProvider client={queryClient}>
 		<App />
