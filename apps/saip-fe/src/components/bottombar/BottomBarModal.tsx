6e526e1ed09d6aefcdee38506cc1a7070dc4d17f
lubomirsilny <lubomir.silny.01@gmail.com>
2024-06-27 12:30:22 +0200

    dix

--- apps/saip-fe/src/components/bottombar/BottomBarModal.tsx
+++ apps/saip-fe/src/components/bottombar/BottomBarModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useTranslation} from "react-i18next";
 
 type Props = {
 	onAccept: () => void;
@@ -6,25 +7,26 @@ type Props = {
 };
 
 function BottomBarModal(props: Props) {
+	const { t } = useTranslation();
 	const { onAccept, onDecline } = props;
 
 	return (
 		<div className="background-container rounded-2xl p-6 flex flex-col gap-4">
-			<h5>Naozaj chcete ukončiť aktuálne kolo? Táto akcia je nezvratná.</h5>
+			<h5>{t("bottombar_modal.check") as string}</h5>
 			<div className="flex flex-row gap-4 justify-end">
 				<button
 					type="button"
 					onClick={onDecline}
 					className="button-light font-bold py-2 px-4 rounded-lg disabled:bg-accent-100 disabled:cursor-not-allowed"
 				>
-					Zrušiť
+					{t("buttons.cancel") as string}
 				</button>
 				<button
 					type="submit"
 					onClick={onAccept}
 					className="button-dark font-bold py-2 px-4 rounded-lg disabled:bg-accent-100 disabled:cursor-not-allowed"
 				>
-					Potvrdiť
+					{t("buttons.confirm") as string}
 				</button>
 			</div>
 		</div>
